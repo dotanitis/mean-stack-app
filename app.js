ce2b085adeaaa8b07b6e94935acf5a4c6cf0fe6d
Dotan Hofling <dotanitis@gmail.com>
2017-02-11 23:50:00 +0200

    Add express functionality, listen to a port

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+var express = require('express');
+
+var app = express();
+
+app.set('port',3000);
+
+
+
+var server = app.listen(app.get('port'), function() {
+	var port = server.address().port;
+	console.log('Magic happends on port '+ port );
+});
+
+
+console.log('this is the first')
