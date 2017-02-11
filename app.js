6012f9e8c4f6a3f517af937750d23f21a1d8efaf
Dotan Hofling <dotanitis@gmail.com>
2017-02-12 00:27:59 +0200

    Serves a simple homepage html

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.set('port',3000);
 app.get('/', function(req, res){
 	console.log('GET the homepage');
 	res
-		.status(404)
-		.send("Express yourself :)");
+		.status(200)
+		.sendFile(path.join(__dirname,'public','index.html'));
 });
 
 app.get('/json', function(req, res){
