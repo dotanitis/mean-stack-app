05f35881052c3a57a248799b1743c3d461cedecf
Dotan Hofling <dotanitis@gmail.com>
2017-02-12 00:06:00 +0200

    Add json and file handler

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 var express = require('express');
-
 var app = express();
+var path = require('path');
 
 app.set('port',3000);
 
+app.get('/', function(req, res){
+	console.log('GET the homepage');
+	res
+		.status(404)
+		.send("Express yourself :)");
+});
+
+app.get('/json', function(req, res){
+	console.log('Get the json');
+	res
+		.status(200)
+		.json( {"jsonData" : true});
+});
+
+app.get('/file', function(req, res){
+	console.log('Get the file');
+	res
+		.status(200)
+		.sendFile(path.join(__dirname, 'app.js'));
+});
 
 
 var server = app.listen(app.get('port'), function() {
