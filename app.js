68549bc6594c7a2cbb782611691bb6f1648afb5c
Dotan Hofling <dotanitis@gmail.com>
2017-02-17 00:00:32 +0200

    Add Routers, Controllers and data for hotels

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var routes = require('./api/routes');
 
 app.set('port',3000);
 
+// Middlewere interact with request before the response
+
+app.use(function(req,res,next) {
+	console.log(req.method, req.url);
+	next();
+});
+
 app.use(express.static(path.join(__dirname,'public')));
-
-app.get('/json', function(req, res){
-	console.log('Get the json');
-	res
-		.status(200)
-		.json( {"jsonData" : true});
-});
-
-app.get('/file', function(req, res){
-	console.log('Get the file');
-	res
-		.status(200)
-		.sendFile(path.join(__dirname, 'app.js'));
-});
+app.use('/api',routes)
 
 
 var server = app.listen(app.get('port'), function() {
