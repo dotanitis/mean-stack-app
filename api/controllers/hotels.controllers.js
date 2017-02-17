6494d6f5e421387fb93831d9a4e26b6d8d417906
Dotan Hofling <dotanitis@gmail.com>
2017-02-18 01:40:38 +0200

    Support get single hotel from api

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -6,3 +6,12 @@ module.exports.hotelsGetAll = function(req,res){
 			.status(200)
 			.json( hotelsData )
 };
+
+module.exports.hotelsGetOne = function(req,res){
+	var hotelId = req.params.hotelId;
+	var singleHotel = hotelsData[hotelId];
+	console.log('Get the hotels');
+		res
+			.status(200)
+			.json( singleHotel )
+};
